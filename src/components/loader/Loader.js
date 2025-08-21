79c5f20d3cbf7f79321f94ec137bf5b4b7e622de
daniel rozenblat <danielroz12345@gmail.com>
2025-08-21 23:59:43 +0300

    newwww

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import styles from './Loader.module.css';
 
-const LoadingEffect = () => {
+const Loader = ({ text = "LOADING...", scale = 2 }) => {
+  const letters = text.split('');
+
   return (
-    <div className={styles.container}>
-      <div className={styles.heartContainer}>
-        <div className={styles.heart}></div>
-        <div className={styles.sparkle}></div>
-        <div className={styles.sparkle} style={{ animationDelay: '0.4s' }}></div>
-        <div className={styles.sparkle} style={{ animationDelay: '0.8s' }}></div>
+    <div className={styles.loaderWrapper} style={{ transform: `scale(${scale})` }}>
+      {letters.map((letter, index) => (
+        <span 
+          key={index} 
+          className={styles.loaderLetter}
+          style={{ animationDelay: `${0.1 + index * 0.105}s` }}
+        >
+          {letter}
+        </span>
+      ))}
+      <div className={styles.loader}>
+        <div className={styles.loaderAfter}></div>
       </div>
-      <span className={styles.text}>טוען...</span>
     </div>
   );
 };
 
-export default LoadingEffect;
+export default Loader;
